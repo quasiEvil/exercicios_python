64e7a5b5e72befd7652aa5d50415604145a78f5c
Ana Paula Badaró <thisisanapaula@gmail.com>
2024-01-19 21:39:16 -0300

    minor fix

--- conceitos_iniciais/exercicio04.py
+++ conceitos_iniciais/exercicio04.py
@@ -10,13 +10,13 @@ if salario_bruto <= 1903.98:
     print(f"Seu salário líquido é R$ {salario_bruto:.2f} e é isento de IR.")
 elif salario_bruto >= 1908.99 and salario_bruto <= 2826.65:
     faixa2 = salario_bruto - (salario_bruto * 0.075)
-    print(f"Seu salário líquido é de R$ {faixa2:.2f} e sofre um desconto de 7,5%.")
+    print(f"Seu salário líquido é R$ {faixa2:.2f} e sofre um desconto de 7,5%.")
 elif salario_bruto >= 2826.66 and salario_bruto <= 3751.05:
     faixa3 = salario_bruto - (salario_bruto * 0.15)
-    print(f"Seu salário líquido é de R$ {faixa3:.2f} e sofre um desconto de 15%.")
+    print(f"Seu salário líquido é R$ {faixa3:.2f} e sofre um desconto de 15%.")
 elif salario_bruto >= 3751.06 and salario_bruto <= 4664.68:
     faixa4 = salario_bruto - (salario_bruto * 0.225)
-    print(f"Seu salário líquido é de R$ {faixa4:.2f} e sofre um desconto de 22,5%.")
+    print(f"Seu salário líquido é R$ {faixa4:.2f} e sofre um desconto de 22,5%.")
 elif salario_bruto > 4664.68:
     faixa5 = salario_bruto - (salario_bruto * 0.275)
-    print(f"Seu salário líquido é de R$ {faixa5:.2f} e sofre um desconto de 27,5%.")
+    print(f"Seu salário líquido é R$ {faixa5:.2f} e sofre um desconto de 27,5%.")
