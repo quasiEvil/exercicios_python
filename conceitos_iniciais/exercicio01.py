15600fb2bc67a136494161c340811eb55fd83a0b
Ana Paula Badaró <thisisanapaula@gmail.com>
2024-01-20 16:06:08 -0300

    adds exercise07

--- conceitos_iniciais/exercicio01.py
+++ conceitos_iniciais/exercicio01.py
@@ -2,8 +2,6 @@
 
 from datetime import datetime
 
-print("\nVamos descobrir a sua idade?\n")
-
 ano_nascimento = int(input("Digite seu ano de nascimento: "))
 ano_atual = datetime.now().year
 idade_atual = ano_atual - ano_nascimento
--- conceitos_iniciais/exercicio07.py
+++ conceitos_iniciais/exercicio07.py
@@ -1,2 +1,9 @@
 # Faça um programa que pergunte quanto você ganha por hora e a quantidade de horas trabalhadas no mês.
 # Calcule e mostre o total do salário do referido mês.
+
+valor_hora = float(input("Digite quanto você ganha por hora: R$ "))
+horas_mes = float(input("Digite a quantidade de horas trabalhadas este mês: "))
+salario = valor_hora * horas_mes
+
+print("-----------------------------")
+print(f"\nEste mês seu salário foi R$ {salario:.2f}.\n")
