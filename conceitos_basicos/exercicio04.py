43c694aff1b1a4f323dcd6161943409444dbf6b9
Ana Paula Badaró <thisisanapaula@gmail.com>
2024-01-24 09:02:47 -0300

    adds variables and removes magic numbers

--- conceitos_basicos/exercicio04.py
+++ conceitos_basicos/exercicio04.py
@@ -3,17 +3,24 @@
 
 salario_bruto = float(input("Digite seu salário bruto: R$ "))
 
-if salario_bruto <= 1903.98:
+faixa1 = 1903.98
+faixa2 = 2826.65
+faixa3 = 3751.05
+faixa4 = 4664.68
+faixa5 = 4664.69
+
+
+if salario_bruto <= faixa1:
     print(f"Seu salário líquido é R$ {salario_bruto:.2f} e é isento de IR.")
-elif salario_bruto >= 1908.99 and salario_bruto <= 2826.65:
+elif salario_bruto <= faixa2:
     faixa2 = salario_bruto - (salario_bruto * 0.075)
     print(f"Seu salário líquido é R$ {faixa2:.2f} e sofre um desconto de 7,5%.")
-elif salario_bruto >= 2826.66 and salario_bruto <= 3751.05:
+elif salario_bruto <= faixa3:
     faixa3 = salario_bruto - (salario_bruto * 0.15)
     print(f"Seu salário líquido é R$ {faixa3:.2f} e sofre um desconto de 15%.")
-elif salario_bruto >= 3751.06 and salario_bruto <= 4664.68:
+elif salario_bruto <= faixa4:
     faixa4 = salario_bruto - (salario_bruto * 0.225)
     print(f"Seu salário líquido é R$ {faixa4:.2f} e sofre um desconto de 22,5%.")
-elif salario_bruto > 4664.68:
+else:
     faixa5 = salario_bruto - (salario_bruto * 0.275)
     print(f"Seu salário líquido é R$ {faixa5:.2f} e sofre um desconto de 27,5%.")
